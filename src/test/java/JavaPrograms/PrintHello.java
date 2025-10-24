075c5477acb22759fcdf4f75d7b0b1e1d6bf1025
Raj10siva <rajkumar10siva@gmail.com>
2025-10-24 15:36:15 +0530

    2 java files deleted

--- src/test/java/JavaPrograms/PrintHello.java
+++
@@ -1,9 +0,0 @@
-package JavaPrograms;
-
-public class PrintHello {
-
-	public static void main(String[] args) {
-		//Date - 22/10/2025
-		System.out.println("Hello from Main branch and featureA branch!!!");
-	}
-}
