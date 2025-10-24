5e14595e1c589574e4d4a862372a1bdf628b2396
Raj10siva <rajkumar10siva@gmail.com>
2025-10-24 14:45:00 +0530

    Initial Commit Starting
    "This is the first commit in the main branch"

--- src/test/java/JavaPrograms/PrintHello.java
+++ src/test/java/JavaPrograms/PrintHello.java
@@ -5,16 +5,6 @@ public class PrintHello {
 	public static void main(String[] args) {
 		//Date - 22/10/2025
 		System.out.println("Hello Main!!!");
-		
-		//Date - 23/10/2025
-		System.out.println("Hello Feature 1 is added!!!");
-
-		//Date - 24/10/2025
-		System.out.println("Whatsapp login is added in Feature 1");
-		
-		//Date - 24/10/2025
-		System.out.println("Hello Feature 2 is added!!!");
-
 	}
 
 }
