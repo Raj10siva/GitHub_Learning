f860bf675ad90fbe7f4d762bdbda1128ea80ba3b
Raj10siva <rajkumar10siva@gmail.com>
2025-10-24 15:39:44 +0530

    Updated print statement in feature A branch

--- src/test/java/JavaPrograms/Print.java
+++ src/test/java/JavaPrograms/Print.java
@@ -5,7 +5,7 @@ public class Print {
 	public static void main(String[] args) {
 
 		//Date - 24/10/2025
-		System.out.println("Hello from main branch");
+		System.out.println("Hello from Feature A branch");
 	}
 
 }
