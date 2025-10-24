baadb29266a052f963b6c482a0df1326199f8028
Raj10siva <rajkumar10siva@gmail.com>
2025-10-24 15:10:55 +0530

    Initial Commit
    Just Hello main print statement is added!

--- src/test/java/JavaPrograms/PrintHello.java
+++ src/test/java/JavaPrograms/PrintHello.java
@@ -5,9 +5,6 @@ public class PrintHello {
 	public static void main(String[] args) {
 		//Date - 22/10/2025
 		System.out.println("Hello Main!!!");
-		
-		//Date - 24/10/2025
-		System.out.println("Feature 1 is added");
 	}
 
 }
