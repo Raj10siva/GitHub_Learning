dc49d5230e7fdff2ecb538cc75f9f47d1fb379ff
Raj10siva <rajkumar10siva@gmail.com>
2025-10-24 16:49:45 +0530

    Update Print.java

--- src/test/java/JavaPrograms/Print.java
+++ src/test/java/JavaPrograms/Print.java
@@ -5,7 +5,7 @@ public class Print {
 	public static void main(String[] args) {
 
 		//Date - 24/10/2025
-		System.out.println("Hello from Feature A, B and Main branch");
+		System.out.println("Hello from Feature A, B and Main branch to all");
 		
 		//Adding new line 
 		System.out.println("New line added in the feature B branch");
