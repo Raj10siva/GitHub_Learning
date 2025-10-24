37221c0aa54890d406b4ae3b4f2b7ffbd586e412
Raj10siva <rajkumar10siva@gmail.com>
2025-10-24 15:13:36 +0530

    Initial commit
    Hello main print statement message is added

--- src/test/java/JavaPrograms/PrintHello.java
+++ src/test/java/JavaPrograms/PrintHello.java
@@ -5,12 +5,6 @@ public class PrintHello {
 	public static void main(String[] args) {
 		//Date - 22/10/2025
 		System.out.println("Hello Main!!!");
-		
-		//Date - 24/10/2025
-		System.out.println("Feature 1 is added");
-		
-		//Date - 24/10/2025
-		System.out.println("Hello Main from feature 2 branch");
 	}
 
 }
